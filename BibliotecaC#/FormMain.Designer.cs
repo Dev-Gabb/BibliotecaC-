a15cbe7afb774f32512feea49d2cf30ff49e799a
Gabriel Ferreira <gabrielferreiradoamaral957@gmail.com>
2025-05-21 00:09:19 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaC#/FormMain.Designer.cs
@@ -0,0 +1,414 @@
+﻿namespace BibliotecaC_
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            panel1 = new Panel();
+            panelMenu = new Panel();
+            Inicio = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            panelTopo = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            label2 = new Label();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            panelConteudo = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            pictureBox8 = new PictureBox();
+            label3 = new Label();
+            panelMenu.SuspendLayout();
+            panelTopo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            panelConteudo.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(156, 324);
+            panel1.TabIndex = 0;
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = SystemColors.ActiveCaptionText;
+            panelMenu.Controls.Add(pictureBox7);
+            panelMenu.Controls.Add(pictureBox6);
+            panelMenu.Controls.Add(pictureBox4);
+            panelMenu.Controls.Add(pictureBox3);
+            panelMenu.Controls.Add(pictureBox1);
+            panelMenu.Controls.Add(pictureBox2);
+            panelMenu.Controls.Add(button5);
+            panelMenu.Controls.Add(button6);
+            panelMenu.Controls.Add(button4);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(Inicio);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(165, 450);
+            panelMenu.TabIndex = 2;
+            panelMenu.Paint += this.panelMenu_Paint;
+            // 
+            // Inicio
+            // 
+            Inicio.FlatStyle = FlatStyle.Flat;
+            Inicio.ForeColor = SystemColors.ButtonHighlight;
+            Inicio.ImageAlign = ContentAlignment.MiddleLeft;
+            Inicio.Location = new Point(54, 118);
+            Inicio.Name = "Inicio";
+            Inicio.Size = new Size(75, 36);
+            Inicio.TabIndex = 3;
+            Inicio.Text = "Inicio";
+            Inicio.TextAlign = ContentAlignment.MiddleLeft;
+            Inicio.UseVisualStyleBackColor = true;
+            Inicio.Click += this.button1_Click;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(54, 173);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 36);
+            button2.TabIndex = 4;
+            button2.Text = "Leitores";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(54, 232);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Livros";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(54, 288);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Emprestimos";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(54, 342);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Ajuda";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.ImageAlign = ContentAlignment.MiddleLeft;
+            button6.Location = new Point(54, 415);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 7;
+            button6.Text = "Sair";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // panelTopo
+            // 
+            panelTopo.BackColor = Color.FromArgb(255, 128, 0);
+            panelTopo.Controls.Add(label2);
+            panelTopo.Controls.Add(pictureBox5);
+            panelTopo.Dock = DockStyle.Top;
+            panelTopo.Location = new Point(165, 0);
+            panelTopo.Name = "panelTopo";
+            panelTopo.Size = new Size(635, 79);
+            panelTopo.TabIndex = 3;
+            panelTopo.Paint += this.panelTopo_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox1.Image = Properties.Resources.conversa_em_grupo;
+            pictureBox1.Location = new Point(12, 173);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(35, 36);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += this.pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(590, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 4;
+            label1.Text = "INÍCIO";
+            label1.Click += this.label1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 118);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(35, 36);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox3.Image = Properties.Resources.pilha_de_livros;
+            pictureBox3.Location = new Point(12, 219);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(35, 36);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox4.Image = Properties.Resources.de_dinheiro;
+            pictureBox4.Location = new Point(11, 275);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(36, 36);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox5.Image = Properties.Resources.do_utilizador;
+            pictureBox5.Location = new Point(577, 12);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(55, 46);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 5;
+            pictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(519, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 15);
+            label2.TabIndex = 5;
+            label2.Text = "ADM123";
+            label2.Click += this.label2_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox6.Image = Properties.Resources.sinal_de_interrogacao;
+            pictureBox6.Location = new Point(12, 328);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(37, 36);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox7.Image = Properties.Resources.sair;
+            pictureBox7.Location = new Point(12, 399);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(37, 39);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 5;
+            pictureBox7.TabStop = false;
+            // 
+            // panelConteudo
+            // 
+            panelConteudo.BackColor = Color.FromArgb(255, 255, 192);
+            panelConteudo.Controls.Add(panel4);
+            panelConteudo.Controls.Add(label1);
+            panelConteudo.Controls.Add(panel3);
+            panelConteudo.Controls.Add(panel2);
+            panelConteudo.Dock = DockStyle.Fill;
+            panelConteudo.Location = new Point(165, 79);
+            panelConteudo.Name = "panelConteudo";
+            panelConteudo.Size = new Size(635, 371);
+            panelConteudo.TabIndex = 5;
+            panelConteudo.Paint += this.panelConteudo_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Location = new Point(313, 76);
+            panel2.Name = "panel2";
+            panel2.Padding = new Padding(1);
+            panel2.Size = new Size(258, 91);
+            panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Black;
+            panel3.Location = new Point(180, 185);
+            panel3.Name = "panel3";
+            panel3.Padding = new Padding(1);
+            panel3.Size = new Size(258, 91);
+            panel3.TabIndex = 1;
+            panel3.Paint += this.panel3_Paint;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Black;
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(pictureBox8);
+            panel4.Location = new Point(35, 76);
+            panel4.Name = "panel4";
+            panel4.Padding = new Padding(1);
+            panel4.Size = new Size(258, 91);
+            panel4.TabIndex = 2;
+            panel4.Paint += this.panel4_Paint;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.FromArgb(255, 128, 0);
+            pictureBox8.Image = Properties.Resources.conversa_em_grupo;
+            pictureBox8.Location = new Point(4, 18);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(40, 50);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 5;
+            pictureBox8.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(50, 31);
+            label3.Name = "label3";
+            label3.Size = new Size(195, 23);
+            label3.TabIndex = 6;
+            label3.Text = "Leitores Cadastrados : 5";
+            label3.Click += this.label3_Click;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelConteudo);
+            Controls.Add(panelTopo);
+            Controls.Add(panelMenu);
+            Controls.Add(panel1);
+            Name = "FormMain";
+            Text = "BibliotecaC#";
+            Load += Form1_Load;
+            panelMenu.ResumeLayout(false);
+            panelTopo.ResumeLayout(false);
+            panelTopo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            panelConteudo.ResumeLayout(false);
+            panelConteudo.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panelMenu;
+        private Button button5;
+        private Button button6;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button Inicio;
+        private Panel panelTopo;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Label label2;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Panel panelConteudo;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private PictureBox pictureBox8;
+        private Label label3;
+    }
+}
---
+++ BibliotecaC#/FormMain.cs
@@ -0,0 +1,15 @@
+namespace BibliotecaC_
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
